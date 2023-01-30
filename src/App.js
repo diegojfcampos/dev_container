d9fa1000af2cd76833f04109b412b720329f19f3
diegojfcampos <diegojfcampos@gmail.com>
2023-01-30 04:41:39 +0000

    Creatinf Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+import Footer from './components/layout/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+
+        //If you remove here will render
+        <Route exact path="/" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+     
+      
+    </Router>
   );
 }
 
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
+
+function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+        <div className={styles.logo}> 
+            <Link to="/" className={styles.logo}>DevContainer</Link>
+        </div>
+        <div className={styles.navidv}>
+            <Link to="/" className={styles.navdiv}>Home</Link>
+            <Link to="/contact" className={styles.navdiv}>Contact</Link>
+            <Link to="/about" className={styles.navdiv}>About</Link>
+        </div>
+        
+    </nav>
+  );
+}
+
+export default NavBar;
