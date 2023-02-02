f243eba65b3ac4097899623d2aa2cc4f88159d33
diegojfcampos <diegojfcampos@gmail.com>
2023-02-02 18:37:30 +0000

    Setting responsivity

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
 return (
 <footer className={styles.footer}>
     <div className={styles.footerdiv}>
-        <Link to='http://linkedin.com/in/diegojfcampos' className={styles.footerlink}target="_blank"><img src="/icons8-linkedin-circled-48.png"></img></Link>
-        <Link to='http://github.com/diegojfcampos' className={styles.footerlink} target="_blank"><img src="/icons8-github-48.png"></img></Link>
+        <Link to='http://linkedin.com/in/diegojfcampos' className={styles.footerlink}target="_blank"><img src="../assets/icons8-linkedin-circled-48.png"></img></Link>
+        <Link to='http://github.com/diegojfcampos' className={styles.footerlink} target="_blank"><img src="../assets/icons8-github-48.png"></img></Link>
     </div>
     <p className={styles.copyrights}><span>Dev-Container &copy</span></p>
 
